0ff892d242ee6cc32392d4603553a656f9ba9349
Hyunjun Jeon <hyunjunjeon@Hyunjuns-MacBook-Pro.local>
2016-10-27 15:48:54 +0900

    bottom nav fragments

--- app/src/main/java/com/hustleandswag/earlytwo/MainActivity.java
+++ app/src/main/java/com/hustleandswag/earlytwo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,8 +18,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.hustleandswag.earlytwo.fragment.FriendsFragment;
 import com.hustleandswag.earlytwo.fragment.MyPostsFragment;
 import com.hustleandswag.earlytwo.fragment.MyTopPostsFragment;
+import com.hustleandswag.earlytwo.fragment.ProfileFragment;
 import com.hustleandswag.earlytwo.fragment.RecentPostsFragment;
 
 public class  MainActivity extends BaseActivity {
@@ -84,13 +87,67 @@ public class  MainActivity extends BaseActivity {
                             case R.id.action_cards:
                                 break;
                             case R.id.action_me:
+                                ProfileFragment newFragment = new ProfileFragment();
+                                Bundle args = new Bundle();
+                                //args.putInt(FriendsFragment.ARG_POSITION, position);
+                                newFragment.setArguments(args);
+
+                                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                                // Replace whatever is in the fragment_container view with this fragment,
+                                // and add the transaction to the back stack so the user can navigate back
+                                transaction.replace(R.id.fragment_container, newFragment);
+                                transaction.addToBackStack(null);
+
+                                // Commit the transaction
+                                transaction.commit();
                                 break;
                             case R.id.action_friends:
+                                FriendsFragment newFragment2 = new FriendsFragment();
+                                Bundle args2 = new Bundle();
+                                //args.putInt(FriendsFragment.ARG_POSITION, position);
+                                newFragment2.setArguments(args2);
+
+                                FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+
+                                // Replace whatever is in the fragment_container view with this fragment,
+                                // and add the transaction to the back stack so the user can navigate back
+                                transaction2.replace(R.id.fragment_container, newFragment2);
+                                transaction2.addToBackStack(null);
+
+                                // Commit the transaction
+                                transaction2.commit();
                                 break;
                         }
                         return false;
                     }
                 });
+
+        // fragment by nav
+
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            ProfileFragment firstFragment = new ProfileFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
     }
 
     @Override
